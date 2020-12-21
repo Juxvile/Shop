54bec9997e7a621ae340357ac62d57e6b87348b0
andru <andrugor1994@gmail.com>
2020-12-21 22:24:03 +0300

    fixed

---
+++ src/main/webapp/reactjs/src/components/Product.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+class Product extends React.Component{
+    render() {
+        return (
+            <div className="text-white">Add Product
+            </div>
+        )
+    }
+}
+
+export default Product;
---
+++ src/main/webapp/reactjs/src/components/ProductList.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+class ProductList extends React.Component{
+    render() {
+        return (
+            <div className="text-white">Product List
+            </div>
+        )
+    }
+}
+
+export default ProductList;
