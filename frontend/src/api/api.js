0318a55744582a0a7c34e3cc451bd3543d6c6164
Juxvile <andrugor1994@gmail.com>
2021-04-05 10:53:49 +0300

    fix pom and some js files

--- frontend/src/api/api.js
+++ frontend/src/api/api.js
@@ -1,16 +1,16 @@
 import axios from "axios";
 
 export const getProduct = async () => {
-    const result = await axios.get("http://localhost:8085/product")
+    const result = await axios.get("http://localhost:8080/product")
     return result.data;
 }
 
 export const getOneProduct = async (id) => {
-    const result = await axios.get("http://localhost:8085/product/"+ id)
+    const result = await axios.get("http://localhost:8080/product/"+ id)
     return result.data;
 }
 
 export const getUsers = async () => {
-    const result = await axios.get("http://localhost:8085/users")
+    const result = await axios.get("http://localhost:8080/users")
     return result.data;
 }
