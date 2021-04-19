b02e98d516dfa1fed8e6659e36de1a68b29c821e
Juxvile <andrugor1994@gmail.com>
2021-04-19 19:03:33 +0300

    frontend code add

--- frontend/src/components/Welcome.js
+++ frontend/src/components/Welcome.js
@@ -1,15 +1,12 @@
 import React from 'react';
 import {Jumbotron} from "react-bootstrap";
-import Card from "react-bootstrap/Card";
 
 const Welcome = () => (
-    <Card>
-        <Jumbotron className="bg-dark text-white hello">
-            <h1>Gamer's Shop</h1>
-            <h4>Whatever you want...</h4>
-            <h6>And even more :)</h6>
-        </Jumbotron>
-    </Card>
+    <Jumbotron className="helloJumbotron">
+        <p className="mainText">Gamer's Shop</p>
+        <p className="middleText">Whatever you want...</p>
+        <p className="smallText">And even more :)</p>
+    </Jumbotron>
 );
 
 
